86cfd5ee00bb8fdb2edc6d1a6182196ae37fd246
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2024-09-22 21:48:57 +0100

    Extra fix

--- app/(site)/api/callback/route.ts
+++ app/(site)/api/callback/route.ts
@@ -1,33 +1,33 @@
-import { AuthorizationCode } from 'simple-oauth2'
-import { randomBytes } from 'crypto'
-import { config } from '../../../lib/config'
+import { AuthorizationCode } from "simple-oauth2";
+import { randomBytes } from "crypto";
+import { config } from "../../../../lib/config";
 
 export async function GET(request: Request) {
-  const host = request.headers.get('host')
+  const host = request.headers.get("host");
   const protocol =
-    process.env.NODE_ENV === 'production' ? 'https://' : 'http://'
-  const url = new URL(`https://${host}/${request.url}`)
-  const urlParams = url.searchParams
+    process.env.NODE_ENV === "production" ? "https://" : "http://";
+  const url = new URL(`https://${host}/${request.url}`);
+  const urlParams = url.searchParams;
 
-  const code = urlParams.get('code') as string
+  const code = urlParams.get("code") as string;
 
-  const provider = urlParams.get('provider') as 'github' | 'gitlab'
-  const client = new AuthorizationCode(config(provider))
+  const provider = urlParams.get("provider") as "github" | "gitlab";
+  const client = new AuthorizationCode(config(provider));
   const tokenParams = {
     code,
     redirect_uri: `${protocol}${host}/api/callback?provider=${provider}`,
-  }
+  };
 
   try {
-    const accessToken = await client.getToken(tokenParams)
-    const token = accessToken.token['access_token']
+    const accessToken = await client.getToken(tokenParams);
+    const token = accessToken.token["access_token"];
 
     console.log({
       url: request.url,
       code,
       token,
       provider,
-    })
+    });
 
     return new Response(
       `
@@ -43,28 +43,28 @@ export async function GET(request: Request) {
     `,
       {
         headers: {
-          'Content-Type': 'text/html',
+          "Content-Type": "text/html",
         },
       }
-    )
+    );
   } catch (e) {
-    const error = e as any
+    const error = e as any;
     return new Response(
       `
         <script>
             event.source.postMessage(
                 'authorization:${error.provider}:error:${JSON.stringify(
-                  error
-                )}',
+        error
+      )}',
                 message.origin
             );
         </script>
       `,
       {
         headers: {
-          'Content-Type': 'text/html',
+          "Content-Type": "text/html",
         },
       }
-    )
+    );
   }
 }
